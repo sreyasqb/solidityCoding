600a18714038888fcea017272dec12baf83eee43
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2022-05-18 15:15:51 +0530

    Create first_contract.sol

---
+++ first_contract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+
+contract Inbox{
+    string public message;
+
+    function Inbox(string initalMessage) public {
+        message=initalMessage;
+    }
+    function setMessage(string newMessage) public {
+        message=newMessage;
+    }
+    function getMessage() public view returns (string) {
+        return message;
+    }
+}
