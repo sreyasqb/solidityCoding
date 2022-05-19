865bd1291fd64029e99d71a4dec00f5039dcc3da
sreyasqb <20pt33@psgtech.ac.in>
2022-05-19 22:11:51 +0530

    deployed first contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -18,6 +18,7 @@ beforeEach(async ()=>{
 
 describe('Inbox',()=>{
     it('deploys a contract',()=>{
-        console.log(inbox);
+        // console.log(inbox);
+        assert.ok(inbox.options.address);
     });
 });
